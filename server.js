ce87060149a606e3b3f40a7ef20719283cfa214c
Michael Midleja <michael.midleja@gmail.com>
2018-10-19 19:34:44 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
