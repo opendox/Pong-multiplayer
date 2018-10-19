8b47810bb1530011ddd55230cbc8eda8dcf4d025
Michael Midleja <michael.midleja@gmail.com>
2018-10-19 21:34:45 +0200

    add scoring

--- js/pong.js
+++ js/pong.js
@@ -1,16 +1,20 @@
-let config = {
+let config =
+{
 	type: Phaser.AUTO,
 	width: 1280,
 	height: 720,
 	pixelArt: true,
-	physics: {
+	physics:
+    {
 		default: 'arcade',
-		arcade: {
+		arcade:
+        {
             fps: 60,
             gravity: { y: 0 }
         }
 	},
-	scene: {
+	scene:
+    {
 		preload: preload,
 		create: create,
 		update: update
@@ -18,18 +22,38 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+let ball, scoreTextLeft, scoreTextRight;
+let scoreLeft = 0;
+let scoreRight = 0;
 
-function preload() {
+function preload()
+{
 	this.load.image('ball', 'assets/ball.png');
 }
 
-function create() {
+function create()
+{
     ball = this.physics.add.sprite(512,512,'ball');
     ball.body.collideWorldBounds=true;
-    ball.setVelocity(1,1);
+    ball.body.bounce.set(1);
+    ball.setVelocity(100,10);
+    
+    scoreTextRight = this.add.text(game.canvas.width-55, 17, ''+scoreRight, { fontSize: '64px', fill: '#ff0044' });
+    scoreTextLeft = this.add.text(17, 17, ''+scoreLeft, { fontSize: '64px', fill: '#ff0044' });
 }
 
 function update ()
 {
-
+    if (ball.body.x < 5)
+    {
+        scoreRight++;
+        scoreTextRight.setText(''+scoreRight);
+        ball.setPosition(512,512);
+    }
+    else if (ball.body.x > game.canvas.width-ball.width-5)
+    {
+        scoreLeft++;
+        scoreTextLeft.setText(''+scoreLeft);
+        ball.setPosition(512,512);
+    }
 }
