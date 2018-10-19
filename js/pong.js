2652729a5cf1845eef23081078d710c4010f791e
Rafael Coelho <rafael.abreu.coelho@gmail.com>
2018-10-19 16:22:05 -0300

    moving the left player and bounciness

--- js/pong.js
+++ js/pong.js
@@ -1,7 +1,11 @@
+
+let windowWidth = 1000;
+let windowHeight = 600;
+
 let config = {
 	type: Phaser.AUTO,
-	width: 1280,
-	height: 720,
+	width: windowWidth,
+	height: windowHeight,
 	pixelArt: true,
 	physics: {
 		default: 'arcade',
@@ -17,19 +21,89 @@ let config = {
 	}
 };
 
+let playerLeft, playerRight;
+
+let playerSpeed = 50;
+let playerYDrag = 200;
+
 let game = new Phaser.Game(config);
 
-function preload() {
+let ballPos = new Phaser.Math.Vector2(windowWidth / 2, windowHeight / 2);
+let ballVel = new Phaser.Math.Vector2(-300, 5);
+
+// Distance the players are from the edge
+let playerMargin = 100;
+
+// Paddle dimensions
+let playerWidth = 20;
+let playerHeight = 100;
+
+// Spawn Positions
+let playerLeftPos = new Phaser.Math.Vector2
+(
+	playerMargin,
+	windowHeight / 2 - (playerHeight / 2)
+);
+let playerRightPos = new Phaser.Math.Vector2
+(
+	windowWidth - (playerWidth + playerMargin),
+	windowHeight / 2 - (playerHeight / 2)
+);
+
+function preload()
+{
 	this.load.image('ball', 'assets/ball.png');
+	this.load.image('paddle', 'assets/paddle.png');
 }
 
-function create() {
-    ball = this.physics.add.sprite(512,512,'ball');
-    ball.body.collideWorldBounds=true;
-    ball.setVelocity(1,1);
+function create()
+{
+  // Create ball
+	let ball = this.physics.add.sprite(512,512,'ball');
+  ball.body.collideWorldBounds=true;
+	ball.setPosition(ballPos.x, ballPos.y);
+  ball.setVelocity(ballVel.x, ballPos.y);
+	ball.body.bounce.setTo(1, 1);
+
+	// Player left
+	playerLeft = this.physics.add.sprite(32,96,'paddle');
+	playerLeft.enableBody = true;
+  playerLeft.body.collideWorldBounds = true;
+	playerLeft.body.immovable = true;
+
+	playerLeft.body.drag.y = playerYDrag;
+	playerLeft.setPosition(playerLeftPos.x, playerLeftPos.y);
+	playerLeft.setDisplaySize(playerWidth, playerHeight);
+
+	// Player right
+	playerRight = this.physics.add.sprite(32,96,'paddle');
+	playerRight.enableBody = true;
+	playerRight.body.collideWorldBounds=true;
+	playerRight.body.immovable = true;
+
+	playerRight.body.drag.y = playerYDrag;
+	playerRight.setPosition(playerRightPos.x, playerRightPos.y);
+	playerRight.setDisplaySize(playerWidth, playerHeight);
+
+	this.physics.add.collider(playerLeft, ball);
+	this.physics.add.collider(playerRight, ball);
 }
 
 function update ()
+{
+		cursors = this.input.keyboard.createCursorKeys();
+
+		if (cursors.up.isDown)
+		{
+			playerLeft.body.setVelocity(0, playerLeft.body.velocity.y + -1 * playerSpeed);
+		}
+		if (cursors.down.isDown)
+		{
+			playerLeft.body.setVelocity(0, playerLeft.body.velocity.y + 1 * playerSpeed);
+		}
+}
+
+function collisionHandler()
 {
 
 }
